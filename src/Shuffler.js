b72d195f0f27c51151a8a315162b90a84454fdc9
Bimochan Shrestha <bmochan@gmail.com>
2021-02-14 01:46:49 +0545

    Add celebration and random selection

--- src/Shuffler.js
+++ src/Shuffler.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Confetti from 'react-dom-confetti';
 import TextLoop from 'react-text-loop';
 
 import { DEFAULT_NAMES, LOOP_CONFIG } from './constants';
@@ -9,19 +10,64 @@ export default function Shuffler({
 	animationSpeed = 400,
 	changeRoute,
 }) {
-  const [members, setMembers] = useState(names);
-
+	const [members, setMembers] = useState(names);
+	const [isSpinning, setIsSpinning] = useState(false);
+	const [result, setResult] = useState(null);
+	const [showResult, setShowResult] = useState(false);
+	const [disableSpinButton, setDisableDrawButton] = useState(false);
+	const [pastDrawnMembers, setPastDrawnMembers] = useState([]);
 	useEffect(() => {
 		const storedMembers = getMembersFromStorage();
-    setMembers(storedMembers);
-	},[]);
+		setMembers(storedMembers);
+	}, []);
+
+	const sleep = (time) => {
+		return new Promise((resolve) => setTimeout(resolve, time));
+	};
+
+	const spin = () => {
+		setIsSpinning(true);
+		setShowResult(false);
+		setDisableDrawButton(true);
+
+		let maxItemIndex = members.length;
+		const randomIndex = Math.floor(Math.random() * maxItemIndex);
+		sleep(3000).then(() => {
+			setShowResult(true);
+			setIsSpinning(false);
+			setResult(members[randomIndex]);
+			setPastDrawnMembers([...pastDrawnMembers, members[randomIndex]]);
+			setDisableDrawButton(false);
+		});
+	};
 
 	return (
 		<div className="d-Flex flex-column">
-			<TextLoop interval={animationSpeed} springConfig={LOOP_CONFIG} children={members} />
-			<button type="button" onClick={changeRoute} name="save" className="save-button">
-				Change names
-			</button>
+			<div>
+			{!showResult && isSpinning &&
+				<TextLoop
+					interval={animationSpeed}
+					springConfig={LOOP_CONFIG}
+					children={members}
+				/>
+      }
+				<Confetti active={showResult} />
+				{showResult && result}
+			</div>
+			<div className="flex-row">
+				<button
+					type="button"
+					onClick={spin}
+					name="spin"
+					className="save-button"
+					disabled={disableSpinButton}
+				>
+					Spin
+				</button>
+				<button type="button" onClick={changeRoute} name="save" className="save-button">
+					Change names
+				</button>
+			</div>
 		</div>
 	);
 }
